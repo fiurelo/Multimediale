57a9c17ebf9a255de5e6bdfa26e7cd3d6599791c
fury <yura.tataryn998@gmail.com>
2023-03-01 12:19:51 +0100

    modifica classe

--- src/multimediale/Audio.java
+++ src/multimediale/Audio.java
@@ -8,6 +8,29 @@ package multimediale;
  *
  * @author fury
  */
-public class Audio {
+public class Audio extends Multimediale {
+        int durata,volume;
+
+    public Audio(int durata, int volume) {
+        this.durata = durata;
+        this.volume = volume;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public void setDurata(int durata) {
+        this.durata = durata;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
     
+        
 }
